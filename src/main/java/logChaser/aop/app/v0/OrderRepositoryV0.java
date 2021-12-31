542db044872f59f038a3edbc027db8afb007e08e
myunghoonju <myunghoonju@gmail.com>
2022-01-01 02:25:26 +0900

    create project

---
+++ src/main/java/logChaser/aop/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,10 @@
+package logChaser.aop.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+}
