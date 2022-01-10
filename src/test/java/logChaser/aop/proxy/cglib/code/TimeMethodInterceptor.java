3bf93602602556436273a8e86127cf8cf5dca607
myunghoonju <myunghoonju@gmail.com>
2022-01-10 16:43:27 +0900

    CglibTest

---
+++ src/test/java/logChaser/aop/proxy/cglib/code/TimeMethodInterceptor.java
@@ -0,0 +1,32 @@
+package logChaser.aop.proxy.cglib.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeMethodInterceptor implements MethodInterceptor {
+
+    private final Object target;
+
+    public TimeMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        log.info("TimeMethodInterceptor start");
+
+        long startTime = System.currentTimeMillis();
+
+        Object result = methodProxy.invoke(target, args);
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("End TimeMethodInterceptor = resultTime = {} ms", resultTime);
+        return result;
+    }
+}
---
+++ src/test/java/logChaser/aop/proxy/common/service/ServiceInterface.java
@@ -0,0 +1,8 @@
+package logChaser.aop.proxy.common.service;
+
+public interface ServiceInterface {
+
+    void save();
+    void find();
+
+}
